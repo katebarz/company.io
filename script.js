0fabed0d0910e07270ffe82365c7a3c01237b48c
katebarz <k.barzakovska@gmail.com>
2018-11-22 14:04:45 +0100

    menu button + responsive

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 menu.onclick = function myFunction(){
-	var x = document.getElementById('myTopnav')
+	let x = document.getElementById('myTopnav')
+	let menu = document.getElementById('menu')
+
 
 	if(x.className === "top-nav"){
 		x.className += " responsive"
+		menu.className+=" responsive"
 	}
 	else{
 		x.className = "top-nav"
+		menu.className = "menu-toggle"
 	}
 }
